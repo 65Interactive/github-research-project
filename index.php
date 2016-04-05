5a69af5956a43f4937c5d525c0627527aaf9d269
lvgelderen <coppertop_@live.ca>
2016-04-05 01:31:54 -0400

    Update index.php
    added js and css style sheet links

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Welcome to the Finest Selection of Blu-rays on the internets!</title>
+<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 <?php
@@ -49,5 +50,6 @@ echo	'<div id="1" class="movieGallery"></div>
 	include('includes/footer.html');
 	
 ?>
+<script src="main.js"></script>
 </body>
 </html>
