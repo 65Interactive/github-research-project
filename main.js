ff6c76b09b05c44b54a9eec07957b8bd2831befe
lvgelderen <coppertop_@live.ca>
2016-04-05 00:20:22 -0400

    finished js
    added a current variable that changes a current image to full opacity.

--- main.js
+++ main.js
@@ -8,6 +8,9 @@
   setInterval(function(){
     for ( var i=0; i < slider.length; i++){
       slider[i].style.opacity = 0;
+    }
+    current = (current != slider.length - 1) ? currnt + 1 : 0;
+    slider[i].style.opacity = 1;
   }, 3000);
 
 })();
